9a9a6caf0fdfe2a872338abda60fdfe40e1c31b1
Lukas Elmer <lukas.elmer@gmail.com>
2017-12-14 17:39:56 +0100

    Day 10 part 2

--- 10/run.ts
+++ 10/run.ts
@@ -1,24 +1,30 @@
 import * as fs from "fs";
 
 export function day10() {
-  console.log(`Example: ${knotHash([3, 4, 1, 5], [0, 1, 2, 3, 4]).result()}`);
-  console.log(`Part 1: ${knotHash(readInput(), generateNumbers()).result()}`);
+  console.log(
+    `Example: ${singleKnotHash([3, 4, 1, 5], [0, 1, 2, 3, 4]).part1Result()}`
+  );
+  const inNums = readInput()
+    .split(",")
+    .map(Number);
+  console.log(
+    `Part 1: ${singleKnotHash(inNums, generateNumbers()).part1Result()}`
+  );
+  console.log(`Part 2: ${fullKnotHash(readInput(), generateNumbers())}`);
 }
 
 function readInput() {
   return fs
     .readFileSync("./10/input.txt")
     .toString()
-    .trim()
-    .split(",")
-    .map(Number);
+    .trim();
 }
 
 function generateNumbers() {
   return [...Array(256).keys()];
 }
 
-function knotHash(input: number[], state: number[]) {
+function singleKnotHash(input: number[], state: number[]) {
   const hashState = new HashState(state);
   input.forEach(num => hashState.mutate(num));
   return hashState;
@@ -49,9 +55,51 @@ class HashState {
     this.skipSize++;
   }
 
-  result() {
+  part1Result() {
     return this.state[0] * this.state[1];
   }
+}
+
+function fullKnotHash(input: string, state: number[]) {
+  const magic = [17, 31, 73, 47, 23];
+  const convertedSequence = input
+    .split("")
+    .map(convertFromAscii)
+    .concat(magic);
+
+  const hashState = new HashState(state);
+  for (let i = 0; i < 64; i++)
+    convertedSequence.forEach(num => hashState.mutate(num));
+
+  return denseHash(state);
+}
+
+function convertFromAscii(char: string) {
+  return char.charCodeAt(0);
+}
+
+function denseHash(state: number[]) {
+  const chunks = inChunksOf(state, 16);
+  return chunks
+    .map(xor)
+    .map(toHex)
+    .join("");
+}
+
+function inChunksOf(state: number[], chunkSize: number) {
+  const stateCopy = state.slice(0);
+  const results = [];
+  while (stateCopy.length > 0) results.push(stateCopy.splice(0, chunkSize));
+  return results;
+}
+
+function xor(block: number[]): number {
+  return block.reduce((result, num) => result ^ num, 0);
+}
+
+function toHex(num: number): string {
+  const hex = num.toString(16);
+  return hex.length === 1 ? `0${hex}` : hex;
 }
 
 day10();
