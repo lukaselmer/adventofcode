1ff6a0d75299ae9bda79a7475f239dda1185bd4a
Lukas Elmer <lukas.elmer@gmail.com>
2017-12-18 00:17:15 +0100

    Day 15 part 2

--- 15/run.ts
+++ 15/run.ts
@@ -3,13 +3,25 @@ import { sum, generateN } from "../utils";
 const GENERATOR_A_FACTOR = 16807;
 const GENERATOR_B_FACTOR = 48271;
 
+const GENERATOR_A_PICKY = 4;
+const GENERATOR_B_PICKY = 8;
+
 class Generator {
   static GENERATOR_DIVISION = 2147483647;
 
-  constructor(public state: number, private factor: number) {}
+  constructor(
+    public state: number,
+    private factor: number,
+    private picky: number = 1
+  ) {}
 
   next() {
     this.state = (this.state * this.factor) % Generator.GENERATOR_DIVISION;
+  }
+
+  nextPicky() {
+    this.next();
+    while (this.state % this.picky !== 0) this.next();
   }
 
   get lowest16Bits() {
@@ -20,6 +32,8 @@ class Generator {
 export function day15() {
   console.log(`Example part 1: ${countMatches(65, 8921)}`);
   console.log(`Part 1: ${countMatches(516, 190)}`);
+  console.log(`Example part 2: ${countPickyMatches(65, 8921)}`);
+  console.log(`Part 2: ${countPickyMatches(516, 190)}`);
 }
 
 function countMatches(seedA: number, seedB: number) {
@@ -29,6 +43,26 @@ function countMatches(seedA: number, seedB: number) {
     generateN(40 * 1000 * 1000).map(() => {
       generatorA.next();
       generatorB.next();
+      return matchFirst16Bits(generatorA, generatorB) ? 1 : 0;
+    })
+  );
+}
+
+function countPickyMatches(seedA: number, seedB: number) {
+  const generatorA = new Generator(
+    seedA,
+    GENERATOR_A_FACTOR,
+    GENERATOR_A_PICKY
+  );
+  const generatorB = new Generator(
+    seedB,
+    GENERATOR_B_FACTOR,
+    GENERATOR_B_PICKY
+  );
+  return sum(
+    generateN(5 * 1000 * 1000).map(() => {
+      generatorA.nextPicky();
+      generatorB.nextPicky();
       return matchFirst16Bits(generatorA, generatorB) ? 1 : 0;
     })
   );
