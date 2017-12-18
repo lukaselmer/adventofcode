a58778f7a168b3a8cce7a29174d30f0fd26bd8ae
Lukas Elmer <lukas.elmer@gmail.com>
2017-12-18 01:18:28 +0100

    Day 17 part 2

--- 17/run.ts
+++ 17/run.ts
@@ -3,18 +3,45 @@ import { generateN } from "../utils";
 export function day17() {
   console.log(`Example part 1: ${spinlockShortCircuit(3)}`);
   console.log(`Part 1: ${spinlockShortCircuit(359)}`);
+  console.log(`Part 2: ${spinlockValueAfterZero(359)}`);
 }
 
 function spinlockShortCircuit(steps: number) {
+  const result = spinSpinlock(2017, steps);
+  return result.spinlock[result.currentIndex + 2];
+}
+
+function spinSpinlock(spins: number, steps: number) {
   const spinlock: number[] = [0];
   let currentIndex = 0;
-  generateN(2017)
+  generateN(spins)
     .map(value => value + 1)
     .forEach(value => {
       currentIndex = (currentIndex + steps + 1) % spinlock.length;
       spinlock.splice(currentIndex + 1, 0, value);
     });
-  return spinlock[currentIndex + 2];
+  return { spinlock, currentIndex };
+}
+
+/*
+brute force...
+function spinlockValueAfterZero(steps: number) {
+  const result = spinSpinlock(50000000 - 1, steps);
+  const indexOfZero = result.spinlock.indexOf(0);
+  return result.spinlock[indexOfZero + 1];
+}
+*/
+
+function spinlockValueAfterZero(steps: number) {
+  let currentIndex = 0;
+  let lastAfter0 = 0;
+  generateN(50000000 - 1)
+    .map(value => value + 1)
+    .forEach(value => {
+      currentIndex = (currentIndex + steps + 1) % value;
+      if (currentIndex === 0) lastAfter0 = value;
+    });
+  return lastAfter0;
 }
 
 day17();
