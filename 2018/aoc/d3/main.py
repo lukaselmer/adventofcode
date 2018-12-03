858a2501508f5c256ac5e152b0b9bfe5cfdbc420
Lukas Elmer <lukas.elmer@gmail.com>
2018-12-03 23:50:26 +0100

    Day 3 part 2

--- 2018/aoc/d3/main.py
+++ 2018/aoc/d3/main.py
@@ -5,25 +5,20 @@ from dataclasses import dataclass
 
 
 def multi_claimed_square_inches():
-    fabric = Fabric(1000)
-    for claim in _read_input():
-        fabric.fill(claim)
+    fabric = _fill_fabric()
     return fabric.count_multi_claimed_square_inches()
 
 
-def _read_input():
-    with open("aoc/d3/input.txt") as file:
-        for line in file:
-            if line:
-                yield _parse_claim(line.strip())
+def find_intact_claim():
+    fabric = _fill_fabric()
+    return fabric.find_intact_claim()
 
 
-def _parse_claim(line: str):
-    clean_line = line.replace(":", "").replace("@ ", "").replace("#", "")
-    [identifier, coords, dim] = clean_line.split(" ")
-    [x_cord, y_cord] = map(int, coords.split(","))
-    [width, height] = map(int, dim.split("x"))
-    return Claim(identifier=int(identifier), top_left=(x_cord, y_cord), dimensions=(width, height))
+def _fill_fabric():
+    fabric = Fabric(1000)
+    for claim in _read_input():
+        fabric.fill(claim)
+    return fabric
 
 
 @dataclass
@@ -68,9 +63,36 @@ class Fabric:
                     count += 1
         return count
 
+    def find_intact_claim(self):
+        valid_claims: Set[int] = set([claim.identifier for claim in _read_input()])
+        for row in self._rows:
+            for cell in row:
+                if len(cell) > 1:
+                    for claim_identifier in cell:
+                        if claim_identifier in valid_claims:
+                            valid_claims.remove(claim_identifier)
+        assert len(valid_claims) == 1
+        return valid_claims.pop()
+
     def __repr__(self):
         return "\n".join(["".join(map(str, row)) for row in self._rows])
 
 
+def _read_input():
+    with open("aoc/d3/input.txt") as file:
+        for line in file:
+            if line:
+                yield _parse_claim(line.strip())
+
+
+def _parse_claim(line: str):
+    clean_line = line.replace(":", "").replace("@ ", "").replace("#", "")
+    [identifier, coords, dim] = clean_line.split(" ")
+    [x_cord, y_cord] = map(int, coords.split(","))
+    [width, height] = map(int, dim.split("x"))
+    return Claim(identifier=int(identifier), top_left=(x_cord, y_cord), dimensions=(width, height))
+
+
 if __name__ == "__main__":
     print(multi_claimed_square_inches())
+    print(find_intact_claim())
--- 2018/aoc/d3/test.py
+++ 2018/aoc/d3/test.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import mock_open, patch
 
-from aoc.d3.main import multi_claimed_square_inches
+from aoc.d3.main import find_intact_claim, multi_claimed_square_inches
 
 
 class ActuatorTestCase(unittest.TestCase):
@@ -9,6 +9,11 @@ class ActuatorTestCase(unittest.TestCase):
         data = "#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2"
         with patch("builtins.open", mock_open(read_data=data)):
             self.assertEqual(4, multi_claimed_square_inches())
+
+    def test_find_intact_claim(self):
+        data = "#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2"
+        with patch("builtins.open", mock_open(read_data=data)):
+            self.assertEqual(3, find_intact_claim())
 
 
 if __name__ == "__main__":
