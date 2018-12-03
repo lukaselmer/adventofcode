6fc7e7a7599d64e82c13abaea4119ce775f0940b
Lukas Elmer <lukas.elmer@gmail.com>
2018-12-03 21:22:29 +0100

    Day 1 part 2

--- 2018/aoc/d1/main.py
+++ 2018/aoc/d1/main.py
@@ -1,15 +1,28 @@
-def run():
+def sum_up():
     total = 0
     for number in read_input():
         total += number
     return total
 
 
+def first_repetition():
+    total = 0
+    frequencies = set([0])
+    while True:
+        for number in read_input():
+            total += number
+            if total in frequencies:
+                return total
+            frequencies.add(total)
+
+
 def read_input():
     with open("aoc/d1/input.txt") as file:
         for line in file:
-            yield int(line.strip())
+            if line:
+                yield int(line.strip())
 
 
 if __name__ == "__main__":
-    print(run())
+    print(sum_up())
+    print(first_repetition())
--- 2018/aoc/d1/test.py
+++ 2018/aoc/d1/test.py
@@ -1,13 +1,29 @@
 import unittest
 from unittest.mock import mock_open, patch
 
-from aoc.d1.main import run
+from aoc.d1.main import first_repetition, sum_up
 
 
 class ActuatorTestCase(unittest.TestCase):
-    def test_run(self):
+    def test_sum_up(self):
         with patch("builtins.open", mock_open(read_data="+10\n-15\n+7")):
-            self.assertEqual(2, run())
+            self.assertEqual(2, sum_up())
+
+    def test_first_repetition_a(self):
+        with patch("builtins.open", mock_open(read_data="+1\n-1")):
+            self.assertEqual(0, first_repetition())
+
+    def test_first_repetition_b(self):
+        with patch("builtins.open", mock_open(read_data="+3\n+3\n+4\n-2\n-4")):
+            self.assertEqual(10, first_repetition())
+
+    def test_first_repetition_c(self):
+        with patch("builtins.open", mock_open(read_data="-6\n+3\n+8\n+5\n-6")):
+            self.assertEqual(5, first_repetition())
+
+    def test_first_repetition_d(self):
+        with patch("builtins.open", mock_open(read_data="+7\n+7\n-2\n-7\n-4")):
+            self.assertEqual(14, first_repetition())
 
 
 if __name__ == "__main__":
