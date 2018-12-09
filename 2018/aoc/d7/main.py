4e7aafe0b91b3c795409def87a7a702f13412ca6
Lukas Elmer <lukas.elmer@gmail.com>
2018-12-09 12:48:57 +0100

    day 7 part 2

--- 2018/aoc/d7/main.py
+++ 2018/aoc/d7/main.py
@@ -6,23 +6,11 @@ from typing import DefaultDict, List, Set, Tuple
 
 def step_order():
     assembly_steps: List[Tuple[str, str]] = list(_read_input())
-    requirements = defaultdict(set)
-    following = defaultdict(set)
-    for before, after in assembly_steps:
-        requirements[after].add(before)
-        following[before].add(after)
-
-    starting_steps = set()
-    for before, after in assembly_steps:
-        if not requirements[before]:
-            starting_steps.add(before)
-        if not requirements[after]:
-            starting_steps.add(after)
-
-    return "".join(_assemble_sledge(requirements, following, starting_steps))
+    requirements, following, starting_steps = _prepare_datastructures(assembly_steps)
+    return "".join(_assemble_sledge_simple(requirements, following, starting_steps))
 
 
-def _assemble_sledge(
+def _assemble_sledge_simple(
     requirements: DefaultDict[str, Set[str]],
     following: DefaultDict[str, Set[str]],
     starting_steps: Set[str],
@@ -36,6 +24,64 @@ def _assemble_sledge(
             requirements[next_step].remove(current_step)
             if not requirements[next_step]:
                 starting_steps.add(next_step)
+
+
+def time_required(seconds_per_step: int, workers: int):
+    assembly_steps: List[Tuple[str, str]] = list(_read_input())
+    requirements, following, starting_steps = _prepare_datastructures(assembly_steps)
+    return _assemble_sledge_concurrent(
+        seconds_per_step, workers, requirements, following, starting_steps
+    )
+
+
+def _assemble_sledge_concurrent(
+    fixed_seconds_per_step: int,
+    workers: int,
+    requirements: DefaultDict[str, Set[str]],
+    following: DefaultDict[str, Set[str]],
+    starting_steps: Set[str],
+):
+    working: List[Tuple[str, int]] = []
+    seconds_passed = 0
+    while starting_steps or working:
+        if starting_steps and len(working) < workers:
+            current_step: str = sorted(starting_steps)[0]
+            starting_steps.remove(current_step)
+            until = seconds_passed + fixed_seconds_per_step + _seconds_per_step(current_step)
+            working.append((current_step, until))
+        else:
+            seconds_passed = min([until for _, until in working])
+            finishing = [step for step, until in working if until == seconds_passed]
+            working = [(step, until) for step, until in working if until != seconds_passed]
+
+            for current_step in finishing:
+                for next_step in following[current_step]:
+                    requirements[next_step].remove(current_step)
+                    if not requirements[next_step]:
+                        starting_steps.add(next_step)
+
+    return seconds_passed
+
+
+def _seconds_per_step(letter: str):
+    return ord(letter) - ord("A") + 1
+
+
+def _prepare_datastructures(assembly_steps: List[Tuple[str, str]]):
+    requirements: DefaultDict[str, Set[str]] = defaultdict(set)
+    following: DefaultDict[str, Set[str]] = defaultdict(set)
+    for before, after in assembly_steps:
+        requirements[after].add(before)
+        following[before].add(after)
+
+    starting_steps = set()
+    for before, after in assembly_steps:
+        if not requirements[before]:
+            starting_steps.add(before)
+        if not requirements[after]:
+            starting_steps.add(after)
+
+    return requirements, following, starting_steps
 
 
 def _read_input():
@@ -53,3 +99,4 @@ def _read_input():
 
 if __name__ == "__main__":
     print(step_order())
+    print(time_required(60, 5))
--- 2018/aoc/d7/test.py
+++ 2018/aoc/d7/test.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import mock_open, patch
 
-from aoc.d7.main import step_order
+from aoc.d7.main import step_order, time_required
 
 DATA = "Step C must be finished before step A can begin.\nStep C must be finished before step F can begin.\nStep A must be finished before step B can begin.\nStep A must be finished before step D can begin.\nStep B must be finished before step E can begin.\nStep D must be finished before step E can begin.\nStep F must be finished before step E can begin.\n"
 
@@ -10,6 +10,10 @@ class TestCase(unittest.TestCase):
     def test_step_order(self):
         with patch("builtins.open", mock_open(read_data=DATA)):
             self.assertEqual("CABDFE", step_order())
+
+    def test_time_required(self):
+        with patch("builtins.open", mock_open(read_data=DATA)):
+            self.assertEqual(15, time_required(0, 2))
 
 
 if __name__ == "__main__":
