5cdfeb106adb1e9a4e28d0086ffce3ee32d80ac2
Lukas Elmer <lukas.elmer@gmail.com>
2018-12-12 14:58:36 +0100

    day 10 part 1

---
+++ 2018/aoc/d10/main.py
@@ -0,0 +1,96 @@
+from __future__ import annotations
+
+from typing import List, Tuple
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Light:
+    coords: Tuple[int, int]
+    velocity: Tuple[int, int]
+
+    def step(self):
+        self.coords = (self.coords[0] + self.velocity[0], self.coords[1] + self.velocity[1])
+
+    def unstep(self):
+        self.coords = (self.coords[0] - self.velocity[0], self.coords[1] - self.velocity[1])
+
+
+class Simulator:
+    lights: List[Light]
+    last_field_size = 999_999_999
+
+    def __init__(self):
+        self.lights = _parse_coordinates()
+
+    def __str__(self):
+        x_min = min([light.coords[0] for light in self.lights])
+        y_min = min([light.coords[1] for light in self.lights])
+        x_max = max([light.coords[0] for light in self.lights])
+        y_max = max([light.coords[1] for light in self.lights])
+        matrix = [["." for _ in range(x_min, x_max + 1)] for _ in range(y_min, y_max + 1)]
+        for light in self.lights:
+            matrix[light.coords[1] - y_min][light.coords[0] - x_min] = "#"
+        return "\n".join(["".join(row) for row in matrix])
+
+    def run(self):
+        while not self._interesting_lights():
+            # print(".", end="", flush=True)
+            self.simulate_step()
+        self.unsimulate_step()
+        print()
+        print()
+        print(self)
+        print()
+        print()
+
+    def simulate_step(self):
+        for light in self.lights:
+            light.step()
+
+    def unsimulate_step(self):
+        for light in self.lights:
+            light.unstep()
+
+    def _interesting_lights(self):
+        x_min = min([light.coords[0] for light in self.lights])
+        y_min = min([light.coords[1] for light in self.lights])
+        x_max = max([light.coords[0] for light in self.lights])
+        y_max = max([light.coords[1] for light in self.lights])
+        field_size = x_max - x_min + y_max - y_min
+        if self.last_field_size < field_size:
+            return True
+
+        self.last_field_size = field_size
+        return False
+
+
+def _parse_coordinates() -> List[Light]:
+    content = list(_read_input())
+    return [_parse_coordinate(numbers) for numbers in content]
+
+
+def _parse_coordinate(numbers: Tuple[int, int, int, int]):
+    x_coord, y_coord, x_velocity, y_velocity = numbers
+    return Light(coords=(x_coord, y_coord), velocity=(x_velocity, y_velocity))
+
+
+def _read_input():
+    with open("aoc/d10/input.txt") as file:
+        for line in file.readlines():
+            if line.strip():
+                simlified = " ".join(
+                    line.strip()
+                    .replace("position=<", " ")
+                    .replace("> velocity=<", " ")
+                    .replace(">", "")
+                    .replace(",", "")
+                    .strip()
+                    .split()
+                )
+                yield tuple(map(int, simlified.split(" ")))
+
+
+if __name__ == "__main__":
+    Simulator().run()
---
+++ 2018/aoc/d10/test.py
@@ -0,0 +1,30 @@
+import unittest
+from unittest.mock import mock_open, patch
+
+from aoc.d10.main import Simulator
+
+
+def readlines(filename):
+    with open(filename) as file:
+        lines = [line.strip() for line in file.readlines() if line.strip()]
+        return "\n".join(lines)
+
+
+class TestCase(unittest.TestCase):
+    def test_initial_step(self):
+        step_output = readlines("aoc/d10/step-0.txt")
+        with patch("builtins.open", mock_open(read_data=readlines("aoc/d10/example.txt"))):
+            simulator = Simulator()
+            self.assertEqual(step_output, str(simulator))
+
+    def test_simulation(self):
+        step_outputs = [readlines(f"aoc/d10/step-{step}.txt") for step in range(1, 5)]
+        with patch("builtins.open", mock_open(read_data=readlines("aoc/d10/example.txt"))):
+            simulator = Simulator()
+            for step_output in step_outputs:
+                simulator.simulate_step()
+                self.assertEqual(step_output, str(simulator))
+
+
+if __name__ == "__main__":
+    unittest.main()
