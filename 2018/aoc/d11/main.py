8e577a97a1902ae90c21ff5dbb9f57df54532c28
Lukas Elmer <lukas.elmer@gmail.com>
2018-12-13 07:59:21 +0100

    simple strategy

--- 2018/aoc/d11/main.py
+++ 2018/aoc/d11/main.py
@@ -4,36 +4,49 @@ from typing import List, Tuple
 
 
 def largest_grid_value_coords(serial_number: int):
-    matrix = [
-        [Cell(serial_number=serial_number, position=(x_coord, y_coord)) for y_coord in range(1, 302)]
-        for x_coord in range(1, 302)
-    ]
-
-    maximum = ((-1, -1), -1_000_000)
-    for x_coord in range(1, 299):
-        for y_coord in range(1, 299):
-            current_sum = _sum_in_square(matrix, (x_coord, y_coord))
-            if maximum[1] < current_sum:
-                maximum = ((x_coord + 1, y_coord + 1), current_sum)
+    maximum = ((-1, -1, -1), -1_000_000)
+    for size in range(1, 301):
+        print(size, end=" ", flush=True)
+        current = _largest_grid_value_coords_of(_matrix(serial_number), size)
+        if maximum[1] < current[1]:
+            maximum = ((current[0][0], current[0][1], size), current[1])
 
     return maximum[0]
 
 
-def _sum_in_square(matrix: List[List[Cell]], position):
+def largest_grid_value_coords_of_3x3(serial_number: int):
+    return _largest_grid_value_coords_of(_matrix(serial_number), 3)[0]
+
+
+def _matrix(serial_number: int):
+    return [
+        [
+            _calculate_value(serial_number=serial_number, position=(x_coord, y_coord))
+            for y_coord in range(1, 302)
+        ]
+        for x_coord in range(1, 302)
+    ]
+
+
+def _largest_grid_value_coords_of(matrix: List[List[int]], size: int):
+    maximum = ((-1, -1), -1_000_000)
+    for x_coord in range(0, 302 - size):
+        for y_coord in range(0, 302 - size):
+            current_sum = _sum_in_square(matrix, (x_coord, y_coord), size)
+            if maximum[1] < current_sum:
+                maximum = ((x_coord + 1, y_coord + 1), current_sum)
+
+    return maximum
+
+
+def _sum_in_square(matrix: List[List[int]], position: Tuple[int, int], size: int):
     return sum(
         [
-            matrix[position[0] + x_coord][position[1] + y_coord].value
-            for x_coord in range(0, 3)
-            for y_coord in range(0, 3)
+            matrix[position[0] + x_coord][position[1] + y_coord]
+            for x_coord in range(0, size)
+            for y_coord in range(0, size)
         ]
     )
-
-
-class Cell:
-    value: int
-
-    def __init__(self, serial_number: int, position: Tuple[int, int]):
-        self.value = _calculate_value(serial_number, position)
 
 
 def _calculate_value(serial_number: int, position: Tuple[int, int]):
@@ -50,4 +63,4 @@ def _calculate_value(serial_number: int, position: Tuple[int, int]):
 
 
 if __name__ == "__main__":
-    print(largest_grid_value_coords(9445))
+    print(largest_grid_value_coords_of_3x3(9445))
--- 2018/aoc/d11/test.py
+++ 2018/aoc/d11/test.py
@@ -1,19 +1,25 @@
 import unittest
 from unittest.mock import mock_open, patch
 
-from aoc.d11.main import Cell, largest_grid_value_coords
+from aoc.d11.main import _calculate_value, largest_grid_value_coords, largest_grid_value_coords_of_3x3
 
 
 class TestCase(unittest.TestCase):
     def test_cell_value(self):
-        self.assertEqual(4, Cell(8, (3, 5)).value)
-        self.assertEqual(-5, Cell(57, (122, 79)).value)
-        self.assertEqual(0, Cell(39, (217, 196)).value)
-        self.assertEqual(4, Cell(71, (101, 153)).value)
+        self.assertEqual(4, _calculate_value(8, (3, 5)))
+        self.assertEqual(-5, _calculate_value(57, (122, 79)))
+        self.assertEqual(0, _calculate_value(39, (217, 196)))
+        self.assertEqual(4, _calculate_value(71, (101, 153)))
 
-    def test_max_grid_value(self):
-        self.assertEqual((33, 45), largest_grid_value_coords(18))
-        self.assertEqual((21, 61), largest_grid_value_coords(42))
+    def test_max_grid_value_of_3x3(self):
+        self.assertEqual((33, 45), largest_grid_value_coords_of_3x3(18))
+        self.assertEqual((21, 61), largest_grid_value_coords_of_3x3(42))
+
+    def test_max_grid_value_of_any_size_18(self):
+        self.assertEqual((90, 269, 16), largest_grid_value_coords(18))
+
+    def test_max_grid_value_of_any_size_42(self):
+        self.assertEqual((232, 251, 12), largest_grid_value_coords(42))
 
 
 if __name__ == "__main__":
