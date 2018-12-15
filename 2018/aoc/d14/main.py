3e48a6e58b8f2d147fe7c6571f6aa1e5e4c522f8
Lukas Elmer <lukas.elmer@gmail.com>
2018-12-15 04:07:35 +0100

    performance improvements

--- 2018/aoc/d14/main.py
+++ 2018/aoc/d14/main.py
@@ -1,49 +1,48 @@
-from array import array
-
-
 def time_to_produce(goal_recipies_str: str):
     elf_position_a = 4
     elf_position_b = 6
     recipies = [3, 7, 1, 0, 1, 0, 1]
-    # recipies = array("h", [3, 7, 1, 0, 1, 0, 1])
-    index = 0
-    # goal_a, goal_b, goal_c, goal_d, goal_e, goal_f = list(map(int, list(goal_recipies_str)))
+    index = 2
     goal_a, goal_b, goal_c, goal_d, goal_e, goal_f = list(map(int, list(goal_recipies_str)))
     length = len(recipies)
+    prev = 0
     while True:
         recipie = recipies[elf_position_a] + recipies[elf_position_b]
         if recipie >= 10:
             recipies.append(1)
             length += 1
-        recipies.append(recipie % 10)
+
+            if (
+                prev == 0
+                and recipies[index] == goal_a
+                and recipies[index + 1] == goal_b
+                and recipies[index + 2] == goal_c
+                and recipies[index + 3] == goal_d
+            ):
+                return index
+            prev = 1
+            index += 1
+
+        short_recipie = recipie % 10
+        recipies.append(short_recipie)
         length += 1
+
+        if (
+            prev == 0
+            and recipie == 1
+            and recipies[index] == goal_a
+            and recipies[index + 1] == goal_b
+            and recipies[index + 2] == goal_c
+            and recipies[index + 3] == goal_d
+        ):
+            return index
+        index += 1
+        prev = short_recipie
+
         elf_position_a += recipies[elf_position_a] + 1
         elf_position_a %= length
         elf_position_b += recipies[elf_position_b] + 1
         elf_position_b %= length
-
-        if (
-            recipies[index] == goal_a
-            and recipies[index + 1] == goal_b
-            and recipies[index + 2] == goal_c
-            and recipies[index + 3] == goal_d
-            and recipies[index + 4] == goal_e
-            and recipies[index + 5] == goal_f
-        ):
-            return index
-        index += 1
-
-        if recipie >= 10:
-            if (
-                recipies[index] == goal_a
-                and recipies[index + 1] == goal_b
-                and recipies[index + 2] == goal_c
-                and recipies[index + 3] == goal_d
-                and recipies[index + 4] == goal_e
-                and recipies[index + 5] == goal_f
-            ):
-                return index
-            index += 1
 
 
 # def scores_after(num_recipies: int):
