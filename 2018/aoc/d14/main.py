ec11f435a74ff1de6fa2c9d0b65347e8e7e464c1
Lukas Elmer <lukas.elmer@gmail.com>
2018-12-15 03:08:21 +0100

    performance improvements

--- 2018/aoc/d14/main.py
+++ 2018/aoc/d14/main.py
@@ -4,14 +4,27 @@ from typing import List, Tuple
 
 
 def time_to_produce(goal_recipies_str: str):
-    elf_positions = (0, 1)
-    recipies = [3, 7]
+    elf_positions = (4, 6)
+    recipies = [3, 7, 1, 0, 1, 0, 1]
     index = 0
     goal_recipies = list(map(int, list(goal_recipies_str)))
     length = len(goal_recipies)
     while True:
-        elf_positions = _make_recipies(elf_positions, recipies)
-        while len(recipies) >= index + length + 2:
+        number_of_new_recipies, elf_positions = _make_recipies(elf_positions, recipies)
+        if number_of_new_recipies == 1:
+            for offset in range(0, length):
+                if recipies[index + offset] != goal_recipies[offset]:
+                    break
+                if offset == length - 1:
+                    return index
+            index += 1
+        else:
+            for offset in range(0, length):
+                if recipies[index + offset] != goal_recipies[offset]:
+                    break
+                if offset == length - 1:
+                    return index
+            index += 1
             for offset in range(0, length):
                 if recipies[index + offset] != goal_recipies[offset]:
                     break
@@ -24,7 +37,7 @@ def scores_after(num_recipies: int):
     elf_positions = (0, 1)
     recipies = [3, 7]
     while len(recipies) <= num_recipies + 12:
-        elf_positions = _make_recipies(elf_positions, recipies)
+        _, elf_positions = _make_recipies(elf_positions, recipies)
     return "".join([str(score) for score in recipies[num_recipies : num_recipies + 10]])
 
 
@@ -35,8 +48,11 @@ def _make_recipies(elf_positions: Tuple[int, int], recipies: List[int]):
     recipies.append(recipie % 10)
 
     return (
-        (elf_positions[0] + recipies[elf_positions[0]] + 1) % len(recipies),
-        (elf_positions[1] + recipies[elf_positions[1]] + 1) % len(recipies),
+        2 if recipie >= 10 else 1,
+        (
+            (elf_positions[0] + recipies[elf_positions[0]] + 1) % len(recipies),
+            (elf_positions[1] + recipies[elf_positions[1]] + 1) % len(recipies),
+        ),
     )
 
 
