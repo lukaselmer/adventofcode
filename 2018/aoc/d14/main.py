9939e565e3b112f836cd32357129327d5ba6076f
Lukas Elmer <lukas.elmer@gmail.com>
2018-12-15 02:54:44 +0100

    day 14 part 2

--- 2018/aoc/d14/main.py
+++ 2018/aoc/d14/main.py
@@ -3,6 +3,20 @@ from __future__ import annotations
 from typing import List, Tuple
 
 
+def time_to_produce(goal_recipies: str):
+    elf_positions = (0, 1)
+    recipies = [3, 7]
+    index = 0
+    length = len(goal_recipies)
+    while True:
+        elf_positions = _make_recipies(elf_positions, recipies)
+        while len(recipies) >= index + length + 2:
+            current_recipies = "".join([str(score) for score in recipies[index : index + length]])
+            if goal_recipies == current_recipies:
+                return index
+            index += 1
+
+
 def scores_after(num_recipies: int):
     elf_positions = (0, 1)
     recipies = [3, 7]
@@ -25,3 +39,4 @@ def _make_recipies(elf_positions: Tuple[int, int], recipies: List[int]):
 
 if __name__ == "__main__":
     print(scores_after(825401))
+    print(time_to_produce("825401"))
--- 2018/aoc/d14/test.py
+++ 2018/aoc/d14/test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from aoc.d14.main import scores_after
+from aoc.d14.main import scores_after, time_to_produce
 
 
 class TestCase(unittest.TestCase):
@@ -15,6 +15,18 @@ class TestCase(unittest.TestCase):
 
     def test_scores_after_2018(self):
         self.assertEqual("5941429882", scores_after(2018))
+
+    def test_time_to_produce_51589(self):
+        self.assertEqual(9, time_to_produce("51589"))
+
+    def test_time_to_produce_01245(self):
+        self.assertEqual(5, time_to_produce("01245"))
+
+    def test_time_to_produce_92510(self):
+        self.assertEqual(18, time_to_produce("92510"))
+
+    def test_time_to_produce_59414(self):
+        self.assertEqual(2018, time_to_produce("59414"))
 
 
 if __name__ == "__main__":
