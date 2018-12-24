04848ae88af4b109d9354a2f443fe718233e7a15
Lukas Elmer <lukas.elmer@gmail.com>
2018-12-24 02:07:29 +0100

    Day 21 part 2

--- 2018/aoc/d21/annotated_input_v4.py
+++ 2018/aoc/d21/annotated_input_v4.py
@@ -1,20 +1,27 @@
 # pylint: disable=C0103
 
+from typing import Set
 
-def main(magic_number: int):
+
+def main(magic_number: int = -1):
     r3 = r4 = 0
+    magic_numbers: Set[int] = set()
+    last_magic_number = None
 
     while True:
         r4 = r3 | 65536
-        r3 = 10649702
+        r3 = 10_649_702
         while True:
-            r3 = (((r3 + (r4 & 255)) & 16777215) * 65899) & 16777215
+            r3 = (((r3 + (r4 & 255)) & 16_777_215) * 65899) & 16_777_215
             if r4 >= 256:
                 r4 = determine_r4(r4)
             else:
-                print(r3)
+                if r3 in magic_numbers:
+                    return last_magic_number
+                last_magic_number = r3
+                magic_numbers.add(r3)
                 if magic_number == r3:
-                    return
+                    return last_magic_number
                 break
 
 
@@ -27,4 +34,4 @@ def determine_r4(r4: int):
 
 
 if __name__ == "__main__":
-    main(10504829)
+    print(main(-1))
--- 2018/aoc/d21/main.py
+++ 2018/aoc/d21/main.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 
 from aoc.d19.main import Simulator, _read_instructions
+from aoc.d21.annotated_input_v4 import main as part2
 
 
 def simulate(filename: str):
     instructions = _read_instructions(filename, "21")
-    simulator = Simulator(*instructions, 0)
+    simulator = Simulator(*instructions, 10504829)
     simulator.watchlist = 28
     while simulator.points_to_instruction:
         simulator.run_instruction()
@@ -13,4 +14,5 @@ def simulate(filename: str):
 
 
 if __name__ == "__main__":
-    print(simulate("input"))
+    simulate("input")
+    print(part2())
