99b9e0af87b2bf6b224fbb5052af101bd1e0a026
Lukas Elmer <lukas.elmer@gmail.com>
2018-12-26 23:48:31 +0100

    refactoring

--- 2018/aoc/d22/main.py
+++ 2018/aoc/d22/main.py
@@ -2,47 +2,51 @@ from __future__ import annotations
 
 from typing import Dict, Tuple
 
+from dataclasses import dataclass
+
 Coordinate = Tuple[int, int]
+
+# def shortest_path(depth: int, target: Coordinate):
+#     checked = {(0, 0)}
 
 
 def total_risk_level(depth: int, target: Coordinate):
-    geo_indices: Dict[Coordinate, int] = dict()
-    geo_indices[(0, 0)] = 0
-    geo_indices[target] = 0
-    coordinates = [(x, y) for x in range(target[0] + 1) for y in range(target[1] + 1)]
-
-    # If the region's Y coordinate is 0, the geo index is its X coordinate times 16807.
-    for x in range(1, target[0] + 1):
-        geo_indices[(x, 0)] = x * 16807
-
-    # If the region's X coordinate is 0, the geo index is its Y coordinate times 48271.
-    for y in range(1, target[1] + 1):
-        geo_indices[(0, y)] = y * 48271
-
-    # Otherwise, the region's geo index is the result of multiplying the erosion levels of the regions at X-1,Y and X,Y-1.
-    # X-1,Y and X,Y-1
-    for coord in coordinates:
-        _geo_index(depth, geo_indices, coord)
-
-    risk_levels: Dict[Coordinate, int] = dict()
-    for coord, geo_index in geo_indices.items():
-        risk_levels[coord] = _erosion_levels(depth, geo_index) % 3
-
-    return sum([risk_levels[coord] for coord in coordinates])
+    cave = Cave(depth, target, dict())
+    coords = [(x, y) for x in range(target[0] + 1) for y in range(target[1] + 1)]
+    risk_levels = {coord: _risk_level(cave, coord) for coord in coords}
+    return sum([risk_levels[coord] for coord in coords])
 
 
-def _geo_index(depth: int, geo_indices: Dict[Coordinate, int], coord: Coordinate):
-    if coord not in geo_indices:
+def _risk_level(cave: Cave, coord: Coordinate):
+    geo_index = _geo_index(cave, coord)
+    return _erosion_levels(cave, geo_index) % 3
+
+
+def _geo_index(cave: Cave, coord: Coordinate):
+    if coord not in cave.geo_indices:
+        if coord in {cave.target, (0, 0)}:
+            return 0
+
         x, y = coord
-        geo_index_a = _geo_index(depth, geo_indices, (x - 1, y))
-        geo_index_b = _geo_index(depth, geo_indices, (x, y - 1))
-        geo_indices[coord] = _erosion_levels(depth, geo_index_a) * _erosion_levels(depth, geo_index_b)
-    return geo_indices[coord]
+
+        # If the region's Y coordinate is 0, the geo index is its X coordinate times 16807.
+        if y == 0:
+            return x * 16807
+
+        # If the region's X coordinate is 0, the geo index is its Y coordinate times 48271.
+        if x == 0:
+            return y * 48271
+
+        # Otherwise, the region's geo index is the result of multiplying the erosion levels of the regions at X-1,Y and X,Y-1.
+        geo_index_a = _geo_index(cave, (x - 1, y))
+        geo_index_b = _geo_index(cave, (x, y - 1))
+        cave.geo_indices[coord] = _erosion_levels(cave, geo_index_a) * _erosion_levels(cave, geo_index_b)
+    return cave.geo_indices[coord]
 
 
 # A region's erosion level is its geo index plus the cave system's depth, all modulo 20183. Then:
-def _erosion_levels(depth: int, geo_index: int):
-    return (geo_index + depth) % 20183
+def _erosion_levels(cave: Cave, geo_index: int):
+    return (geo_index + cave.depth) % 20183
 
 
 def _print_risk_levels(risk_levels: Dict[Coordinate, int], target: Coordinate):
@@ -60,6 +64,13 @@ def _print_risk_levels(risk_levels: Dict[Coordinate, int], target: Coordinate):
                 type_char = "T"
             print(type_char, end="")
         print()
+
+
+@dataclass
+class Cave:
+    depth: int
+    target: Coordinate
+    geo_indices: Dict[Coordinate, int]
 
 
 if __name__ == "__main__":
--- 2018/aoc/d22/test.py
+++ 2018/aoc/d22/test.py
@@ -7,6 +7,9 @@ class TestCase(unittest.TestCase):
     def test_simulate_1_turn(self):
         self.assertEqual(total_risk_level(depth=510, target=(10, 10)), 114)
 
+    # def test_shortest_path(self):
+    #     self.assertEqual(total_risk_level(depth=510, target=(10, 10)), 45)
+
 
 if __name__ == "__main__":
     unittest.main()
